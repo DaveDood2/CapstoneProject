1e02646080390f5a94ac02959ffd6448c097ee5e
Lucy <59835064+DaveDood2@users.noreply.github.com>
2025-01-16 21:29:25 -0600

    Added backend + validation stuff

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 // 'Import' the Express module instead of http
 import express from "express";
-
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import monsters from "./routers/monsters.js";
+
 // Load environment variables from .env file
 dotenv.config();
 
@@ -9,6 +11,14 @@ dotenv.config();
 const PORT = process.env.PORT || 4040;
 // Initialize the Express application
 const app = express();
+
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
 
 const logging = (request, response, next) => {
   console.log(
@@ -78,6 +88,8 @@ app.get("/weather/:city", (request, response) => {
     city
   });
 });
+
+app.use("/monsters", monsters);
 
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
